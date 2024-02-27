109a15cbbca84834765c922a3a7965c0c98dbfbc
Lucas Eneas <lucaseneasmarco@gmail.com>
2024-02-27 18:23:46 -0300

    First commit

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+
+import './Home.css';
+
+function App() {
+  return (
+    <div>
+      <span>dfsda</span>
+      <span>dfsda</span>
+      <div>fdsfds</div>
+      <div>fdsfds</div>
+      <div>fdsfds</div>
+      <div>fdsfds</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import './Header.css';
+
+function Header(props){
+    return(
+        <container>teste</container>
+    )
+}
+
+export default Header;
